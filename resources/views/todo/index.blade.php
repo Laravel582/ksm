ec21185919225fa28beca49e6186d1c90ad3144b
zailawani laptopp opis <laravel582@gmail.com>
2022-03-22 10:12:12 +0800

    init

---
+++ resources/views/todo/index.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1> my list</h1>
+                <div class="card">
+
+                    <div class="card mt-5">
+                        <div class="col-sm-4">.col-sm-4</div>
+                        <div class="col-sm-4">.col-sm-4</div>
+                        <div class="col-sm-4">.col-sm-4</div>
+                      </div>
+
+                    <div class="card-body">
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>item</th>
+                                    <th>Status</th>
+                                    <th>action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>makan</td>
+                                    <td>not done</td>
+                                    <td><button class="btn btn-primary">Done</button></td>
+                                </tr>
+                                <tr>
+                                    <td>minum</td>
+                                    <td>done</td>
+                                    <td><button class="btn btn-primary">Done</button></td>
+                                </tr>
+                            </tbody>
+                       </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
